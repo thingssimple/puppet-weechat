dd484ed3f73610a3a54d11b128e483e654e3d6ff
Michael Avila <me@michaelavila.com>
2015-12-25 01:32:52 -0800

    Install weechat with all of the things

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,14 @@
 class weechat {
-  anchor { 'Hello_World': }
+  package { 'weechat':
+    ensure => present,
+    provider => brew,
+    install_options => [
+      '--with-curl',
+      '--with-guile',
+      '--with-lua',
+      '--with-perl',
+      '--with-python',
+      '--with-ruby',
+    ]
+  }
 }
